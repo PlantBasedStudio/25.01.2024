036a3b084b48c205916cc5148edc0e4f43ee4e5a
Damien <ultrarushgame@gmail.com>
2024-01-25 12:10:47 +0100

    Cours du jour. Mettre en place son environnement, découverte PyCharm

---
+++ generate_graph.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+# utilisation d'un package Python pour générer une figure
+plt.figure()
+
+plt.plot([1, 2, 3, 4, 5])
+
+plt.show()
+
+# Show all packages installed -> pip list    or     pip freeze
